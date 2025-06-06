03694b78b7717bc0b19cf6ee8e8295c268dce5dc
Yash kasera <yashkasera07@gmail.com>
2025-06-06 15:48:54 +0530

    improve the layout of the dashboard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -8,13 +8,12 @@ import {getInterviewsByUserId, getLatestInterviews} from "@/lib/actions/general.
 
 const Page = async()=> {
   const user = await getCurrentUser();
-  const [userInterviews, allInterview] = await Promise.all([
+  const [userInterviews] = await Promise.all([
     getInterviewsByUserId(user?.id!),
     getLatestInterviews({ userId: user?.id! }),
   ]);
   
   const hasPastInterviews = userInterviews?.length! > 0;
-  const hasUpcomingInterviews = allInterview?.length! > 0;
 
   return (
     <>
@@ -51,25 +50,7 @@ const Page = async()=> {
         </div>
       </section>
 
-      <section className="flex flex-col gap-6 mt-8">
-        <h2>Take Interviews</h2>          
-        <div className="interviews-section">
-          {hasUpcomingInterviews ? (
-            allInterview?.map((interview) => (
-              <InterviewCard
-                key={interview.id}
-                userId={user?.id}
-                role={interview.role}
-                type={interview.type}
-                techstack={interview.techstack}
-                createdAt={interview.createdAt}
-              />
-            ))
-          ) : (
-            <p>There are no interviews available</p>
-          )}
-        </div>
-      </section>
+      
     </>
   );
 }
